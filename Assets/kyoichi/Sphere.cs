5c7111e498a13ff9993e5c147217ce5c80acba2b
Kyoichi <40378788+kyoichi001@users.noreply.github.com>
2020-11-03 19:03:42 +0900

    wall and ball

---
+++ Assets/kyoichi/Sphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Vector3 velocity;
+
+    // Start is called before the first frame update
+    //物体が生成されたときに１回呼ばれる
+    void Start()
+    {
+        rigidbody.AddForce(velocity);
+    }
+
+    // Update is called once per frame
+    //毎フレーム呼ばれる
+    void Update()
+    {
+
+    }
+
+
+
+}
